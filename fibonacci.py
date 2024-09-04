cf77d7948db102033801f280f3b0e96f4dd61855
Denneii <106123685+Denneii@users.noreply.github.com>
2024-09-04 17:01:12 -0300

    Add files via upload

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,14 @@
+#Questão 1 
 def is_fibonacci(n):
-    # Função para verificar se um número é quadrado perfeito
+    
     def e_quadrado_perfeito(x):
         s = int(x ** 0.5)
         return s * s == x
 
-    # um ou ambos de (5*n^2 + 4) ou (5*n^2 - 4) forem quadrados perfeitos.
+    
     return e_quadrado_perfeito(5 * n * n + 4) or e_quadrado_perfeito (5 * n * n - 4)
 
-# Número que queremos verificar
+
 numero = int(input("Informe um número: "))
 
 # Verificando se o número pertence à sequência de Fibonacci
@@ -15,3 +16,27 @@ if is_fibonacci(numero):
     print(f"O número {numero} pertence à sequência de Fibonacci.")
 else:
     print(f"O número {numero} não pertence à sequência de Fibonacci.")
+
+#Questão 2
+def verificar_letra_a(string):
+    
+    count_a = string.count('a')
+    count_A = string.count('A')
+    
+    
+    total_count = count_a + count_A
+    
+    
+    if total_count > 0:
+        print(f"A letra 'a' ocorre {total_count} vez(es) na string.")
+    else:
+        print("A letra 'a' não ocorre na string.")
+
+
+string = input("Informe uma string: ")
+
+
+verificar_letra_a(string)
+
+
+
